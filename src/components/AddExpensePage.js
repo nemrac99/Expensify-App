193e95de64d4b023cd6455850fceb034410c6b0f
nemrac99 <nemrac99@hotmail.com>
2020-12-30 10:43:29 -0600

    EditExpensePage test added

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -3,9 +3,9 @@ import { connect } from "react-redux";
 import ExpenseForm from "./ExpenseForm";
 import { addExpense } from "../actions/expenses";
 
-class AddExpensePage extends React.Component {
+export class AddExpensePage extends React.Component {
   onSubmit = (expense) => {
-    this.props.onSubmit(expense);
+    this.props.addExpense(expense);
     this.props.history.push("/");
   }
   render() {
@@ -13,14 +13,14 @@ class AddExpensePage extends React.Component {
       <div>
       <h1>Add Expense</h1>
       <ExpenseForm 
-        onSubmit = {this.onSubmit}/>
+       onSubmit = {this.onSubmit}/>
     </div>
     )
   }
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  onSubmit: (expense) => dispatch(addExpense(expense))
+  addExpense: (expense) => dispatch(addExpense(expense))
 });
 
 export default connect(undefined, mapDispatchToProps)(AddExpensePage);
--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -1,11 +1,23 @@
 import React from "react";
 import { shallow } from "enzyme";
 import { AddExpensePage } from "../../components/AddExpensePage";
+import expenses from "../fixtures/expenses";
+
+let addExpense, history, wrapper;
+
+beforeEach(() => {
+	addExpense = jest.fn();
+	history = { push: jest.fn() };
+	wrapper = shallow(<AddExpensePage addExpense={addExpense} history={history} />);
+});
 
 test("should render AddExpense page", () => {
-	const onSubmit = jest.fn();
-	const history = { push: jest.fn() };
-	const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
 	expect(wrapper).toMatchSnapshot();
 	// this is a test
+});
+
+test("handle onSubmit", () => {
+	wrapper.find("ExpenseForm").prop("onSubmit")(expenses[1]);
+	expect(history.push).toHaveBeenLastCalledWith("/");
+	expect(addExpense).toHaveBeenCalledWith(expenses[1]);
 });
